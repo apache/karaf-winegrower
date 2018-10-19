7f9b11162a1176323b912f932c4bfc188e9b6db2
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-10-19 15:15:41 +0200

    ensure the javaagent can work from a lib folder until we have an alternate manifest metadata read capability

--- winegrower-extension/winegrower-agent/src/main/java/org/apache/winegrower/extension/agent/WinegrowerAgent.java
+++ winegrower-extension/winegrower-agent/src/main/java/org/apache/winegrower/extension/agent/WinegrowerAgent.java
@@ -21,12 +21,11 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.jar.JarFile;
 import java.util.stream.Stream;
-
-import org.apache.winegrower.Ripener;
-import org.apache.winegrower.scanner.manifest.ManifestContributor;
 
 public class WinegrowerAgent {
     private static final boolean DEBUG = Boolean.getBoolean("winegrower.agent.debug");
@@ -42,77 +41,131 @@ public class WinegrowerAgent {
         if (DEBUG) {
             print("agentargs: " + agentArgs);
         }
+
         ofNullable(extractConfig(agentArgs, "libs="))
             .ifPresent(value -> Stream.of(value.split(","))
-                .map(File::new)
-                .filter(File::exists)
-                .flatMap(it -> it.isDirectory() ?
-                        ofNullable(it.listFiles()).map(Stream::of).orElseGet(Stream::empty) :
-                        Stream.of(it))
-                .filter(it -> it.getName().endsWith(".zip") || it.getName().endsWith(".jar"))
-                .forEach(lib -> {
-                    try {
-                        instrumentation.appendToSystemClassLoaderSearch(new JarFile(lib));
-                    } catch (final IOException e) {
-                        throw new IllegalArgumentException(e);
-                    }
-                }));
+                  .map(File::new)
+                  .filter(File::exists)
+                  .flatMap(it -> it.isDirectory() ?
+                          ofNullable(it.listFiles()).map(Stream::of).orElseGet(Stream::empty) :
+                          Stream.of(it))
+                  .filter(it -> it.getName().endsWith(".zip") || it.getName().endsWith(".jar"))
+                  .forEach(lib -> {
+                      try {
+                          instrumentation.appendToSystemClassLoaderSearch(new JarFile(lib));
+                      } catch (final IOException e) {
+                          throw new IllegalArgumentException(e);
+                      }
+                  }));
 
-        final Ripener ripener = new Ripener.Impl(createConfiguration(agentArgs));
-        ripener.start();
-        System.setProperty("wingrower.agent.started", "true");
-        Runtime.getRuntime().addShutdownHook(new Thread(ripener::stop, WinegrowerAgent.class.getName() + "-shutdown"));
+        try {
+            doStart(agentArgs);
+        } catch (final Throwable e) {
+            throw new IllegalStateException(e);
+        }
     }
 
-    private static Ripener.Configuration createConfiguration(String agentArgs) {
-        final Ripener.Configuration configuration = new Ripener.Configuration();
+    private static void doStart(final String agentArgs) throws Throwable {
+        final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        final Class<?> ripenerImplClass = loader.loadClass("org.apache.winegrower.Ripener$Impl");
+        final Class<?> ripenerConfigurationClass = loader.loadClass("org.apache.winegrower.Ripener$Configuration");
+
+        final Object ripener = ripenerImplClass.getConstructor(ripenerConfigurationClass)
+                                               .newInstance(createConfiguration(ripenerConfigurationClass, agentArgs));
+        doCall(ripener, "start", new Class<?>[0], new Object[0]);
+        System.setProperty("wingrower.agent.started", "true");
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                doCall(ripener, "stop", new Class<?>[0], new Object[0]);
+            } catch (final Throwable throwable) {
+                if (DEBUG) {
+                    throwable.printStackTrace();
+                } // else: not that important
+            }
+        }, WinegrowerAgent.class.getName() + "-shutdown"));
+    }
+
+    private static void doCall(final Object instance, final String mtd, final Class<?>[] paramTypes, final Object[] args) {
+        try {
+            instance.getClass().getMethod(mtd, paramTypes).invoke(instance, args);
+        } catch (final InvocationTargetException ite) {
+            final Throwable targetException = ite.getTargetException();
+            if (RuntimeException.class.isInstance(targetException)) {
+                throw RuntimeException.class.cast(targetException);
+            }
+            throw new IllegalStateException(targetException);
+        } catch (final Exception ex) {
+            if (RuntimeException.class.isInstance(ex)) {
+                throw RuntimeException.class.cast(ex);
+            }
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    private static Object createConfiguration(final Class<?> configType, String agentArgs) throws Throwable {
+        final Object configuration = configType.getConstructor().newInstance();
         ofNullable(extractConfig(agentArgs,"workDir="))
                 .map(String::valueOf)
                 .map(File::new)
-                .ifPresent(configuration::setWorkDir);
+                .ifPresent(value -> doCall(configuration, "setWorkDir", new Class<?>[]{File.class}, new Object[]{value}));
         ofNullable(extractConfig(agentArgs,"prioritizedBundles="))
                 .map(String::valueOf)
                 .filter(it -> !it.isEmpty())
                 .map(it -> asList(it.split(",")))
-                .ifPresent(configuration::setPrioritizedBundles);
+                .ifPresent(value -> doCall(configuration, "setPrioritizedBundles", new Class<?>[]{List.class}, new Object[]{value}));
         ofNullable(extractConfig(agentArgs,"ignoredBundles="))
                 .map(String::valueOf)
                 .filter(it -> !it.isEmpty())
                 .map(it -> asList(it.split(",")))
-                .ifPresent(configuration::setIgnoredBundles);
+                .ifPresent(value -> doCall(configuration, "setIgnoredBundles", new Class<?>[]{Collection.class}, new Object[]{value}));
         ofNullable(extractConfig(agentArgs,"scanningIncludes="))
                 .map(String::valueOf)
                 .filter(it -> !it.isEmpty())
                 .map(it -> asList(it.split(",")))
-                .ifPresent(configuration::setScanningIncludes);
+                .ifPresent(value -> doCall(configuration, "setScanningIncludes", new Class<?>[]{Collection.class}, new Object[]{value}));
         ofNullable(extractConfig(agentArgs,"scanningExcludes="))
                 .map(String::valueOf)
                 .filter(it -> !it.isEmpty())
                 .map(it -> asList(it.split(",")))
-                .ifPresent(configuration::setScanningExcludes);
+                .ifPresent(value -> doCall(configuration, "setScanningExcludes", new Class<?>[]{Collection.class}, new Object[]{value}));
         ofNullable(extractConfig(agentArgs,"manifestContributors="))
                 .map(String::valueOf)
                 .filter(it -> !it.isEmpty())
                 .map(it -> asList(it.split(",")))
                 .ifPresent(contributors -> {
-                    configuration.setManifestContributors(contributors.stream().map(clazz -> {
-                        try {
-                            return Thread.currentThread().getContextClassLoader().loadClass(clazz).getConstructor().newInstance();
-                        } catch (final InstantiationException | NoSuchMethodException | IllegalAccessException
-                                | ClassNotFoundException e) {
-                            throw new IllegalArgumentException(e);
-                        } catch (final InvocationTargetException e) {
-                            throw new IllegalArgumentException(e.getTargetException());
-                        }
-                    }).map(ManifestContributor.class::cast).collect(toList()));
+                    try {
+                        final Class<?> type = Thread.currentThread().getContextClassLoader().loadClass(
+                                    "org.apache.winegrower.scanner.manifest.ManifestContributor");
+                        final Collection<?> value = contributors.stream()
+                                                      .map(clazz -> {
+                                                          try {
+                                                              return Thread.currentThread()
+                                                                           .getContextClassLoader()
+                                                                           .loadClass(clazz)
+                                                                           .getConstructor()
+                                                                           .newInstance();
+                                                          } catch (final InstantiationException | NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
+                                                              throw new IllegalArgumentException(e);
+                                                          } catch (final InvocationTargetException e) {
+                                                              throw new IllegalArgumentException(
+                                                                      e.getTargetException());
+                                                          }
+                                                      })
+                                                      .map(type::cast)
+                                                      .collect(toList());
+                        doCall(configuration, "setManifestContributors", new Class<?>[]{Collection.class}, new Object[]{value});
+                    } catch (final ClassNotFoundException e) {
+                        throw new IllegalArgumentException(e);
+                    }
                 });
         ofNullable(extractConfig(agentArgs,"jarFilter="))
                 .map(String::valueOf)
                 .filter(it -> !it.isEmpty())
                 .ifPresent(filter -> {
                     try {
-                        configuration.setJarFilter((Predicate<String>) Thread.currentThread().getContextClassLoader().loadClass(filter)
-                                                                             .getConstructor().newInstance());
+                        final Predicate<String> predicate = (Predicate<String>) Thread.currentThread()
+                              .getContextClassLoader().loadClass(filter).getConstructor().newInstance();
+                        doCall(configuration, "setJarFilter", new Class<?>[]{Predicate.class}, new Object[]{predicate});
                     } catch (final InstantiationException | NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
                         throw new IllegalArgumentException(e);
                     } catch (final InvocationTargetException e) {
